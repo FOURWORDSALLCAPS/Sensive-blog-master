45f88e7ee798c5cb1a95fc0b25ba3312bec5b2e2
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-07-04 14:32:27 +0300

    Added current changes to other pages

--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -71,11 +85,11 @@ def post_detail(request, slug):
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    tags = Tag.objects.annotate(Count('posts')).order_by('-posts__count')[:5]
+    most_popular_tags = tags[::-1]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    posts = Post.objects.annotate(Count('likes')).prefetch_related('author').order_by('-likes__count')[:5]
+    most_popular_posts = posts[::-1]
 
     context = {
         'post': serialized_post,
@@ -90,11 +104,11 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    tags = Tag.objects.annotate(Count('posts')).order_by('-posts__count')[:5]
+    most_popular_tags = tags[::-1]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    posts = Post.objects.annotate(Count('likes')).prefetch_related('author').order_by('-likes__count')[:5]
+    most_popular_posts = posts[::-1]
 
     related_posts = tag.posts.all()[:20]
 
