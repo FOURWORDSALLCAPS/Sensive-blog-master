00898ba7ca88963f48ca09449f8fd98e0aa741cd
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-07-07 10:36:36 +0300

    Fixed bug with primary key type

--- blog/models.py
+++ blog/models.py
@@ -29,6 +29,7 @@ class PostQuerySet(models.QuerySet):
 
 
 class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
     title = models.CharField('Заголовок', max_length=200)
     text = models.TextField('Текст')
     slug = models.SlugField('Название в виде url', max_length=200)
@@ -65,6 +66,7 @@ class Post(models.Model):
 
 
 class Tag(models.Model):
+    id = models.BigAutoField(primary_key=True)
     title = models.CharField('Тег', max_length=20, unique=True)
 
     class Meta:
@@ -85,6 +87,7 @@ class Tag(models.Model):
 
 
 class Comment(models.Model):
+    id = models.BigAutoField(primary_key=True)
     post = models.ForeignKey(
         'Post',
         on_delete=models.CASCADE,
